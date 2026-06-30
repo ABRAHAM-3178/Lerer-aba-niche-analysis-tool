69a4a1a54a69e3966d6c79393baa0154e1090a35
ABRAHAM-3178 <abrahamlerer3178@gmail.com>
2026-06-30 15:50:28 +0800

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -8,6 +8,7 @@ from .scoring import calculate_10d_scores, assign_weight_level
 from .trend import trend_verification
 from .comment_analysis import analyze_comments
 from .report_generator import generate_excel_report
+from .data_loader import load_keyword_history, load_product_history
 
 __all__ = [
     "parse_aba_csv",
@@ -16,5 +17,7 @@ __all__ = [
     "assign_weight_level",
     "trend_verification",
     "analyze_comments",
-    "generate_excel_report"
+    "generate_excel_report",
+    "load_keyword_history",
+    "load_product_history"
 ]
